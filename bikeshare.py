85c6d9e555466c89bfe5c68a521a172b02311c23
Larissa-Duffy <larissaduffy39@hotmail.com>
2022-05-03 23:08:33 +0100

    style: renamed time_stats function
    Renamed def time_stats to def duration_stats

--- bikeshare.py
+++ bikeshare.py
@@ -97,7 +97,7 @@ def load_data(city, month, day):
     return df
 
 
-def time_stats(df):
+def duration_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
